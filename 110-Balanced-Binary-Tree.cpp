633b839aa9ee05c987817f2b68dd2acbc6275587
Rahul Giri <girirahul2028@gmail.com>
2026-08-13 21:46:44 +0530

    [110] [Time Beats: 100.00%] [Memory Beats: 21.07%] - LeetPush

--- 110-Balanced-Binary-Tree.cpp
+++ 110-Balanced-Binary-Tree.cpp
@@ -9,39 +9,41 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-private:
-    int depth( TreeNode* root){
-        if(root == NULL)return 0;
-        int lh = depth(root ->left);
-        int rh = depth(root -> right);
-        return 1 + max(rh,lh);
-    }
-public:
-    bool isBalanced(TreeNode* root) {
-        // Balanced = at every node, left height and right height differ by at most 1.
-        if(root == NULL) return true;
-        
-        bool left = isBalanced(root -> left);
-        bool right = isBalanced(root -> right);
-        bool current = (abs(depth(root-> left) - depth(root ->right)) <= 1);
-        return (left && right && current);
-
-    }
-};
+ //THIS IS O(N2) SOLUTION
 // class Solution {
-// public:
-//     int heightofBT(TreeNode* root){
-//         if(root == nullptr) return 0;
-//         int lh = heightofBT(root -> left);
-//         if(lh == -1) return -1;
-//         int rh = heightofBT(root -> right);
-//         if(rh == -1) return -1;
-//         if(abs(lh - rh) > 1)return -1;
-//         return 1+max(lh,rh);
+// private:
+//     int depth( TreeNode* root){
+//         if(root == NULL)return 0;
+//         int lh = depth(root ->left);
+//         int rh = depth(root -> right);
+//         return 1 + max(rh,lh);
 //     }
+// public:
 //     bool isBalanced(TreeNode* root) {
-//         int val = heightofBT(root);
-//         return val != -1;
+//         // Balanced = at every node, left height and right height differ by at most 1.
+//         if(root == NULL) return true;
+        
+//         bool left = isBalanced(root -> left);
+//         bool right = isBalanced(root -> right);
+//         bool current = (abs(depth(root-> left) - depth(root ->right)) <= 1);
+//         return (left && right && current);
+
 //     }
 // };
+// THIS IS BETTER INTERVIEW APPROACH
+class Solution {
+public:
+    int heightofBT(TreeNode* root){
+        if(root == nullptr) return 0;
+        int lh = heightofBT(root -> left);
+        if(lh == -1) return -1;
+        int rh = heightofBT(root -> right);
+        if(rh == -1) return -1;
+        if(abs(lh - rh) > 1)return -1;
+        return 1+max(lh,rh);
+    }
+    bool isBalanced(TreeNode* root) {
+        int val = heightofBT(root);
+        return val != -1;
+    }
+};
