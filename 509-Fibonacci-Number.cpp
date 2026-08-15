03127b298364b50fa6f73b6492397f4965d9cafc
Rahul Giri <girirahul2028@gmail.com>
2026-08-15 16:08:29 +0530

    [509] [Time Beats: 77.62%] [Memory Beats: 21.16%] - LeetPush

--- 509-Fibonacci-Number.cpp
+++ 509-Fibonacci-Number.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1) return n ;
-        return fib(n-1)+ fib(n-2);
+        // NORMAL RECURSIVE SOLUTION
+        // if(n <= 1) return n ;
+        // return fib(n-1)+ fib(n-2);
+
+
+        //THIS IS DP SOLUTION BY REDUCING SPACE AND TIME TO 0[N] AND SC => 0[1]
+        int prev = 0;
+        int prev2 = 1;
+        for(int i = 1 ; i <= n ; i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        };
+        return prev;
     }
 };
