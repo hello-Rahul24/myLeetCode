61d56ec88c8f0293cf2f41a54c16f5fbbed71d28
Rahul Giri <girirahul2028@gmail.com>
2026-08-15 18:17:49 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 88.71%] - LeetPush

---
+++ 70-Climbing-Stairs.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // it is a fibonacci question where we apply dp 
+    int climbStairs(int n) {
+        int prev = 1;
+        int prev2 = 1;
+        for(int i = 2; i <= n ; i++){
+            int curr = prev + prev2;
+            prev2 = prev ;
+            prev = curr;
+        }
+        return prev;
+
+    }
+};
