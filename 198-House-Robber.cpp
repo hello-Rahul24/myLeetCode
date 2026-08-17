828f6677e07a5cff11dd7511cfd216b17f9fe99d
Rahul Giri <girirahul2028@gmail.com>
2026-08-17 14:53:13 +0530

    [198] [Time Beats: 6.13%] [Memory Beats: 47.19%] - LeetPush

---
+++ 198-House-Robber.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private: int helper(int index,vector<int>& nums , int n , vector<int> &dp){
+    //base case 
+    if(index >= n){
+        return 0;
+    }
+    if(dp[index] != -1){
+        return dp[index];
+    }
+    //otherwise pick the index
+    //call the left recursion
+    int left =nums[index]+ helper(index + 2, nums, n, dp);
+    //not pick the element
+    //calll the right recursion cause we skipping the current element then we are allowed to choose the very next element
+    int right = helper(index + 1 , nums, n, dp);
+    //now return the max result
+    return dp[index] = max(left, right);
+}
+public:
+    int rob(vector<int>& nums) {
+        // int n = nums.size();
+        // //init dp array
+        // vector<int> dp(n, -1);
+        // int ans = helper(0,nums,n, dp);
+        // return ans;
+
+        // TABULATION METHOD
+        int n = nums.size();
+        vector<int> dp(n+2, 0);
+        // starting from right
+        for(int i = n-1; i >= 0 ; i--){
+            int pick = nums[i] + dp[i + 2];
+            int notpick = dp[i+1];
+            dp[i] = max(pick , notpick);
+        }
+        return dp[0];
+    }
+};
