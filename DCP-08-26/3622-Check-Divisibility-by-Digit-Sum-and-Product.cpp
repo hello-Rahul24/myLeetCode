7146393e8b6c0323f54a17b6571be3d5b3b59b29
Rahul Giri <girirahul2028@gmail.com>
2026-08-22 13:34:59 +0530

    [3622] [Time Beats: 100.00%] [Memory Beats: 68.55%] - LeetPush

---
+++ DCP-08-26/3622-Check-Divisibility-by-Digit-Sum-and-Product.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+       int num = n;
+       int sum = 0;
+       int product = 1;
+        while(num > 0){
+            sum += num % 10;
+            product = product * (num % 10);
+            num = num /10;
+        }
+        if(n % (sum + product) == 0){
+            return true;
+        }
+         return false;
+        
+
+    }
+};
