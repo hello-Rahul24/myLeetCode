a7a27190f546d0920df62c98744aead3e948923d
Rahul Giri <girirahul2028@gmail.com>
2026-09-09 01:02:52 +0530

    [54] [Time Beats: 100.00%] [Memory Beats: 84.46%] - LeetPush

---
+++ 54-Spiral-Matrix.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int top = 0;
+        int right = m-1;
+        int bottom = n-1;
+        int left = 0;
+        vector<int> ans;
+        while(top <= bottom && left <= right){
+            //first print the left to right in top
+            for(int i = left; i <= right ; i++){
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i = top; i <= bottom ; i++){
+                ans.push_back(matrix[i][right]);
+            }
+            right --;
+             // Right -> Left
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Bottom -> Top
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
