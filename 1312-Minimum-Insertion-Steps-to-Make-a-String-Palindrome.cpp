bf36b84551d43157125ce1a292f3dd5f2fae9957
Rahul Giri <girirahul2028@gmail.com>
2026-09-16 00:43:39 +0530

    [1312] [Time Beats: 89.54%] [Memory Beats: 98.33%] - LeetPush

--- 1312-Minimum-Insertion-Steps-to-Make-a-String-Palindrome.cpp
+++ 1312-Minimum-Insertion-Steps-to-Make-a-String-Palindrome.cpp
@@ -14,21 +14,23 @@ public:
         reverse(t.begin(), t.end());
         int n = s.size();
         //vector<vector<int>> dp(n+1,vector<int>(n+1, -1));
-        vector<int> prev(n+1, 0);
+        // vector<int> prev(n+1, 0);
         vector<int> curr(n+1, 0);
         // //base case
         // for(int i = 0 ; i < n ; i++){
         //     prev[]
         // }
         for(int ind1 = 1; ind1 <= n ; ind1 ++){
+            int prev = 0;
             for(int ind2 = 1;ind2 <= n ; ind2 ++){
+                int temp = curr[ind2];
             if(s[ind1-1] == t[ind2-1]){
-              curr[ind2] = 1 + prev[ind2-1];
+                curr[ind2] = 1 + prev;
             }else{
-                curr[ind2]= max(prev[ind2], curr[ind2-1]);
+                curr[ind2]= max(curr[ind2], curr[ind2-1]);
             }
+        prev = temp;
             }
-            prev = curr;
         }
         int val = curr[n];  
         return  n-val;
