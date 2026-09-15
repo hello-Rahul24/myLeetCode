4ccb98e869b01419878fbad71cdd98cf9eab71df
Rahul Giri <girirahul2028@gmail.com>
2026-09-16 00:23:10 +0530

    [1312] [Time Beats: 89.54%] [Memory Beats: 87.64%] - LeetPush

---
+++ 1312-Minimum-Insertion-Steps-to-Make-a-String-Palindrome.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int helper(string s , string t,int ind1,int ind2,vector<vector<int>>& dp){
+        //base case
+        if(ind1 < 0 || ind2 < 0)return 0;
+        if(dp[ind1][ind2] != -1)return dp[ind1][ind2];
+        if(s[ind1] == t[ind2]){
+            return dp[ind1][ind2] = 1 + helper(s, t, ind1-1, ind2-1,dp);
+        }
+        return dp[ind1][ind2]= max(helper(s, t, ind1-1 , ind2, dp), helper(s,t, ind1, ind2-1, dp));
+    }
+    int minInsertions(string s) {
+        string t = s;
+        reverse(t.begin(), t.end());
+        int n = s.size();
+        //vector<vector<int>> dp(n+1,vector<int>(n+1, -1));
+        vector<int> prev(n+1, 0);
+        vector<int> curr(n+1, 0);
+        // //base case
+        // for(int i = 0 ; i < n ; i++){
+        //     prev[]
+        // }
+        for(int ind1 = 1; ind1 <= n ; ind1 ++){
+            for(int ind2 = 1;ind2 <= n ; ind2 ++){
+            if(s[ind1-1] == t[ind2-1]){
+              curr[ind2] = 1 + prev[ind2-1];
+            }else{
+                curr[ind2]= max(prev[ind2], curr[ind2-1]);
+            }
+            }
+            prev = curr;
+        }
+        int val = curr[n];  
+        return  n-val;
+    }
+};
