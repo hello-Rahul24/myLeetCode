44e6fdbe503f493fbcdadf46e9822b1e553e323d
Rahul Giri <girirahul2028@gmail.com>
2026-09-18 01:24:18 +0530

    [714] [Time Beats: 77.89%] [Memory Beats: 72.05%] - LeetPush

--- 714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.cpp
+++ 714-Best-Time-to-Buy-and-Sell-Stock-with-Transaction-Fee.cpp
@@ -7,16 +7,27 @@ public:
         if(dp[ind][buy] != -1){
             return dp[ind][buy];
         }
-
         if(buy){
             return dp[ind][buy] = max(-prices[ind]+helper(prices, fee, ind+1,0,dp),helper(prices,fee,ind+1,1,dp));
         }else{
             return dp[ind][buy] = max(((prices[ind]- fee) + helper(prices,fee, ind+1, 1,dp)), helper(prices,fee,ind+1,0,dp));
         }
     }
+
     int maxProfit(vector<int>& prices, int fee){
     int n = prices.size();
-    vector<vector<int>> dp(n, vector<int>(2, -1));
-    return helper(prices,fee, 0, 1,dp);
+    vector<int>prev(2, 0);
+    vector<int>curr(2,0);
+    for(int ind = n-1 ; ind >= 0 ;ind--){
+        for(int buy = 0 ; buy <= 1 ; buy ++){
+            if(buy){
+                curr[buy] = max(-prices[ind]+prev[0],prev[1]);
+            }else{
+                curr[buy] = max((prices[ind]- fee) + prev[1], prev[0]);
+            }
+        }
+        prev = curr;
+    }
+    return curr[1];
     }
 };
