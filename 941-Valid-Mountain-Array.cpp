fe2dfafc83e99a7392654cfe7eb42924aea1720b
Rahul Giri <girirahul2028@gmail.com>
2026-09-19 11:38:49 +0530

    [941] [Time Beats: 35.92%] [Memory Beats: 55.70%] - LeetPush

---
+++ 941-Valid-Mountain-Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        if(n <3)return false;
+        int peak = 0;
+        int down = 0;
+        for(int i = 1 ; i < n-1 ; i++){
+            if(arr[i] == arr[i-1] || arr[i] == arr[i+1])return false;
+            
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
+                peak++;
+            }else if(arr[i] < arr[i-1] && arr[i] < arr[i+1]){
+                down++;
+            }
+        }
+        return down == 0 ? peak == 1 : false;
+    }
+};
