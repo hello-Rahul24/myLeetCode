ae234d0a6c2b9fd4f399c2c70ea9e820fa589ab5
Rahul Giri <girirahul2028@gmail.com>
2026-09-20 15:27:09 +0530

    [1137] [Time Beats: 100.00%] [Memory Beats: 69.26%] - LeetPush

---
+++ 1137-N-th-Tribonacci-Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0)return 0;
+        int prev = 0;
+        int prev1 = 1;
+        int prev2 = 1;
+        for(int i = 3 ; i <= n; i++){
+            int curr = prev+prev1+prev2;
+            prev = prev1;
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        return prev2;
+    }
+};
