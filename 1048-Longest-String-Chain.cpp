30d26654e9ad9db8bcd1739b3286bbc6e99e1366
Rahul Giri <girirahul2028@gmail.com>
2026-09-23 13:00:27 +0530

    [1048] [Time Beats: 9.06%] [Memory Beats: 8.18%] - LeetPush

--- 1048-Longest-String-Chain.cpp
+++ 1048-Longest-String-Chain.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    bool check(string& a, string& b) {
-        if (b.size() - a.size() != 1)
-            return false;
-
-        int i = 0, j = 0;
-
-        while (i < a.size() && j < b.size()) {
-            if (a[i] == b[j]) {
+    static bool comparator(string word1, string word2){
+        return word1.size() < word2.size();
+    }
+    bool check(string one , string two){
+        if(two.size() - one.size() != 1)return false;
+        int i = 0;
+        int j = 0;
+        while(i < one.size() && j < two.size()){
+            if(one[i] == two[j]){
                 i++;
                 j++;
-            } else {
+            }else{
                 j++;
             }
         }
-
-        return i == a.size();
+       return i == one.size();
     }
     int longestStrChain(vector<string>& words) {
-        // sort the array
-        sort(words.begin(), words.end(),
-     [](string &a, string &b) {
-         return a.size() < b.size();
-     });
+        //we have to sort this array by length
+        sort(words.begin(), words.end(),comparator);
         int n = words.size();
         vector<int> dp(n, 1);
         int maxlen = 1;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[i] < 1 + dp[j] &&
-                    check(words[j],words[i])){
-                    dp[i] = max(dp[i], 1 + dp[j]);
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(check(words[j], words[i])){
+                    if(dp[i] < dp[j]+1){
+                        dp[i] = dp[j] + 1;
+                    }
                 }
             }
-            maxlen = max(maxlen, dp[i]);
+            if(maxlen < dp[i]){
+                maxlen = dp[i];
+            }
         }
         return maxlen;
     }
