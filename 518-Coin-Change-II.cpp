4470b28b12005394847ba60726febdac63dbf0cb
Rahul Giri <girirahul2028@gmail.com>
2026-09-23 21:46:27 +0530

    [518] [Time Beats: 66.86%] [Memory Beats: 49.58%] - LeetPush

--- 518-Coin-Change-II.cpp
+++ 518-Coin-Change-II.cpp
@@ -1,26 +1,25 @@
 class Solution {
-private:
-    int helper(vector<int>& coins, int ind, int amount, vector<vector<int>> &dp){
-        if(ind == 0){
-            if(amount % coins[0] == 0){
-                return 1;
-            }else{
-                return 0;
-            }
-        }
-        if(dp[ind][amount] != -1)return dp[ind][amount];
-        int notpick = helper(coins, ind-1, amount, dp);
-        int pick = 0;
-        if(amount >= coins[ind]){
-            pick = helper(coins,ind, amount-coins[ind], dp);
-        }
-        return dp[ind][amount] = pick + notpick;
-    }
 public:
+    int helper(int ind , int amount, vector<int>& coins,  vector<vector<int>>& dp){
+        if(amount == 0)return 1;
+        if(ind == 0){
+            if(amount % coins[ind]== 0){
+                return 1;
+            }
+            return 0;
+        }
+        if(dp[ind][amount]!= -1)return dp[ind][amount];
+        //not pick
+        int nottake = helper(ind -1, amount, coins, dp);
+        int take = 0;
+        if(amount >= coins[ind]){
+            take = helper(ind, amount- coins[ind], coins, dp);
+        }
+        return dp[ind][amount] = take + nottake;
+    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        return helper(coins, n-1, amount, dp);
+        vector<vector<int>> dp (n, vector<int>(amount+1, -1));
+        return helper(n-1, amount, coins, dp);
     }
 };
