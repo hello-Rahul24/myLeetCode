c3e19b38a2d728e53c66a4d39515fc159e7000a9
Rahul Giri <girirahul2028@gmail.com>
2026-09-23 12:33:04 +0530

    [368] [Time Beats: 75.57%] [Memory Beats: 78.46%] - LeetPush

--- 368-Largest-Divisible-Subset.cpp
+++ 368-Largest-Divisible-Subset.cpp
@@ -3,28 +3,30 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n, 1);
-        vector<int> prev(n,-1);
-         sort(nums.begin(), nums.end()); 
-        int maxi = 1;
-        int maxiInd = 0;
-        for(int i = 1;i < n ; i++){
-            for(int j = 0; j < i ; j++){
-                if(nums[i]%nums[j] == 0 && dp[i] < 1 + dp[j]){
-                    dp[i] = max(dp[i], 1+dp[j]);
-                    prev[i] = j;
+        vector<int> prev(n, -1);
+        sort(nums.begin(), nums.end());
+        int maxlen = 1;
+        int maxIND = 0;
+        for(int i = 1 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                if(nums[i] % nums[j] == 0){
+                    if(dp[i] < dp[j] + 1){
+                        dp[i] = dp[j]+1;
+                        prev[i] = j;
+                    }
                 }
             }
-            if(maxi < dp[i]){
-                maxi = dp[i];
-                maxiInd = i;
+            if(maxlen < dp[i]){
+                maxlen = dp[i];
+                maxIND = i;
             }
         }
-        vector<int> ans;
-        int temp = maxiInd;
-        while(temp != -1){
-            ans.push_back(nums[temp]);
-            temp = prev[temp];
+        vector<int>temp;
+        int dummy = maxIND;
+        while(dummy != -1){
+            temp.push_back(nums[dummy]);
+            dummy = prev[dummy];
         }
-        return ans;
+        return temp;
     }
 };
